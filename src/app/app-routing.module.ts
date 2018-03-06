e7db8e959b587e02b7c97dbeeb12407ad5923a24
AngelBanon <arkhangel15@hotmail.com>
2018-03-06 13:20:38 +0100

    User list created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+    {path:'', redirectTo:'/userComponent', pathMatch: 'full' },
+    {path:'appComponent', component: AppComponent },
+    {path:'userComponent', component: UserComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{ }
